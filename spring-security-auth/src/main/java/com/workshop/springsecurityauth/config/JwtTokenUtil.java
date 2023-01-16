a42e303f4dbedd1baea17116fff7ebd638a4cf50
rocio encinas <chio.ref@gmail.com>
2023-01-16 11:22:20 -0400

    Changes in class

--- spring-security-auth/src/main/java/com/workshop/springsecurityauth/config/JwtTokenUtil.java
+++ spring-security-auth/src/main/java/com/workshop/springsecurityauth/config/JwtTokenUtil.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 public class JwtTokenUtil implements Serializable {
     private String jwtSigningKey = "secret";
     private static final long serialVersionUID = -2550185165626007488L;
-    public static final long JWT_TOKEN_VALIDITY = 5*60*60;
+//    public static final long JWT_TOKEN_VALIDITY = 5*60*60;
 
     @Value("${jwt.secret}")
     private String secret;
