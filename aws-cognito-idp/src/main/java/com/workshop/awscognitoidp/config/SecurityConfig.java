df6d7dfd6be7d9b62f62cdf3428be002feafc5bc
rocio encinas <chio.ref@gmail.com>
2023-01-18 10:17:49 -0400

    JWT validator implementation

--- aws-cognito-idp/src/main/java/com/workshop/awscognitoidp/config/SecurityConfig.java
+++ aws-cognito-idp/src/main/java/com/workshop/awscognitoidp/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.workshop.awscognitoidp.config;
 
+import com.workshop.awscognitoidp.filters.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +18,9 @@ import java.util.List;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public static final String SIGNUP_URL = "/api/users/sign-up";
     public static final String SIGN_IN_URL = "/api/users/sign-in";
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -33,5 +39,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .authenticated())
                 .oauth2ResourceServer()
                 .jwt();
+
+        http.addFilterBefore(jwtRequestFilter, BearerTokenAuthenticationFilter.class);
     }
 }
+
+/** Angular o React (opcional)
+ * PRACTICA - Enviar un pull request hasta el Viernes
+ * Roles: Trainer y Student
+ * Models: Student
+ * Student necesita verificar las notas de sus materias
+ * Rol: Student -> materias -> notas
+ * Rol: Trainer -> students -> subir notas
+ * Rol: Trainer -> trainers -> bootcamp -> subir notas
+ * Rol: Admin -> students -> list
+ * Rol: Admin -> students -> notas
+ * Rol: Admin -> trainers -> list
+ */
---
+++ aws-cognito-idp/src/main/java/com/workshop/awscognitoidp/services/UserAuthenticationService.java
@@ -0,0 +1,101 @@
+package com.workshop.awscognitoidp.services;
+
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.model.*;
+import com.workshop.awscognitoidp.exceptions.CognitoUserException;
+import com.workshop.awscognitoidp.models.UserSignInRequest;
+import com.workshop.awscognitoidp.models.UserSignInResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserAuthenticationService {
+    @Value(value = "${aws.cognito.userPoolId}")
+    private String userPoolId;
+    @Value(value = "${aws.cognito.clientId}")
+    private String clientId;
+
+    @Autowired
+    private AWSCognitoIdentityProvider cognitoClient;
+
+    public UserSignInResponse signIn(UserSignInRequest userSignInRequest) throws CognitoUserException {
+
+        UserSignInResponse userSignInResponse = new UserSignInResponse();
+
+        final Map<String, String> authParams = new HashMap<>();
+        authParams.put("USERNAME", userSignInRequest.getUsername());
+        authParams.put("PASSWORD", userSignInRequest.getPassword());
+
+        final AdminInitiateAuthRequest authRequest = new AdminInitiateAuthRequest();
+        authRequest.withAuthFlow(AuthFlowType.ADMIN_NO_SRP_AUTH).withClientId(clientId)
+                .withUserPoolId(userPoolId).withAuthParameters(authParams);
+
+        try {
+            AdminInitiateAuthResult result = cognitoClient.adminInitiateAuth(authRequest);
+
+            AuthenticationResultType authenticationResult = null;
+
+            if (result.getChallengeName() != null && !result.getChallengeName().isEmpty()) {
+
+                System.out.println("Challenge Name is " + result.getChallengeName());
+
+                if (result.getChallengeName().contentEquals("NEW_PASSWORD_REQUIRED")) {
+                    if (userSignInRequest.getPassword() == null) {
+                        throw new CognitoUserException(
+                                "User must change password " + result.getChallengeName());
+                    }
+
+                    final Map<String, String> challengeResponses = new HashMap<>();
+                    challengeResponses.put("USERNAME", userSignInRequest.getUsername());
+                    challengeResponses.put("PASSWORD", userSignInRequest.getPassword());
+                    // add new password
+                    challengeResponses.put("NEW_PASSWORD", userSignInRequest.getNewPassword());
+
+                    final AdminRespondToAuthChallengeRequest request =
+                            new AdminRespondToAuthChallengeRequest()
+                                    .withChallengeName(ChallengeNameType.NEW_PASSWORD_REQUIRED)
+                                    .withChallengeResponses(challengeResponses)
+                                    .withClientId(clientId).withUserPoolId(userPoolId)
+                                    .withSession(result.getSession());
+
+                    AdminRespondToAuthChallengeResult resultChallenge =
+                            cognitoClient.adminRespondToAuthChallenge(request);
+                    authenticationResult = resultChallenge.getAuthenticationResult();
+
+                    userSignInResponse.setAccessToken(authenticationResult.getAccessToken());
+                    userSignInResponse.setIdToken(authenticationResult.getIdToken());
+                    userSignInResponse.setRefreshToken(authenticationResult.getRefreshToken());
+                    userSignInResponse.setExpiresIn(authenticationResult.getExpiresIn());
+                    userSignInResponse.setTokenType(authenticationResult.getTokenType());
+
+
+                } else {
+                    throw new CognitoUserException(
+                            "User has other challenge " + result.getChallengeName());
+                }
+            } else {
+
+                System.out.println("User has no challenge");
+                authenticationResult = result.getAuthenticationResult();
+
+                userSignInResponse.setAccessToken(authenticationResult.getAccessToken());
+                userSignInResponse.setIdToken(authenticationResult.getIdToken());
+                userSignInResponse.setRefreshToken(authenticationResult.getRefreshToken());
+                userSignInResponse.setExpiresIn(authenticationResult.getExpiresIn());
+                userSignInResponse.setTokenType(authenticationResult.getTokenType());
+            }
+
+        } catch (InvalidParameterException e) {
+            throw new CognitoUserException(e.getErrorMessage());
+        } catch (Exception e) {
+            throw new CognitoUserException(e.getMessage());
+        }
+
+        return userSignInResponse;
+    }
+}
+
