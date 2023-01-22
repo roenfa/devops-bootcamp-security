0f9fb45d245c1798296e1b0d0caac5694b96973f
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-01-21 20:16:31 -0500

    refactor: use default client for  aws cognito identity provider

--- aws-cognito-idp/src/main/java/com/workshop/awscognitoidp/config/CognitoConfig.java
+++ aws-cognito-idp/src/main/java/com/workshop/awscognitoidp/config/CognitoConfig.java
@@ -12,24 +12,13 @@ import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder
 
 @Configuration
 public class CognitoConfig {
-    @Value(value = "${aws.access-key}")
-    private String accessKey;
-    @Value(value = "${aws.access-secret}")
-    private String secretKey;
-    @Value(value = "${aws.default-region}")
-    private String awsRegion;
 
     @Value("${aws.cognito.jwk}")
     private String jwkUrl;
 
     @Bean
     public AWSCognitoIdentityProvider cognitoClient() {
-
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        return AWSCognitoIdentityProviderClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(awsRegion)
-                .build();
+        return AWSCognitoIdentityProviderClientBuilder.defaultClient();
     }
 
     @Bean
