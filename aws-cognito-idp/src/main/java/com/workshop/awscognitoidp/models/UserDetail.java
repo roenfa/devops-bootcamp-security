2d21f1341dcf30d327ad39a864b09924e29dd6d0
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-01-22 22:02:39 -0500

    refactor: remove UserDeatils implementation

--- aws-cognito-idp/src/main/java/com/workshop/awscognitoidp/models/UserDetail.java
+++ aws-cognito-idp/src/main/java/com/workshop/awscognitoidp/models/UserDetail.java
@@ -9,45 +9,10 @@ import java.util.Collection;
 
 @Getter
 @Setter
-public class UserDetail implements UserDetails {
+public class UserDetail {
 
     private String firstName;
     private String lastName;
     private String email;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 
 }
